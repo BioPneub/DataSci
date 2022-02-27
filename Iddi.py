5e9711ca2ceaa692a997bd2fdb6f279007acb283
BioPneub <bobombrat@gmail.com>
2022-02-27 15:24:01 -0800

    Iddi
    Iddi Problems

---
+++ Iddi.py
@@ -0,0 +1,33 @@
+#Some fucked up problems you wanted....
+
+####################
+
+#Print the variable x
+x = 10
+
+print('x')
+
+####################
+
+#How would you get the items in the list to print without printing duplicates?
+
+list = ['Moms', 'Spaghetti', 'Knees','Weak','Weak','Knees','Spaghetti','Moms']
+print(list)
+
+####################
+
+#Using the same list, how would you print only 'Moms'?
+
+list = ['Moms', 'Spaghetti', 'Knees','Weak','Weak','Knees','Spaghetti','Moms']
+
+####################
+
+#Write an exception to handle the integers below 10 so that there is a printed statement for each element in the list.
+
+y = [1,6,9,10,16,14]
+
+for numbers in y:
+    if numbers > 10:
+        print("It's bigger than 10")
+
+####################
